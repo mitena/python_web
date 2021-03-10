5e3bc6a0deceafed5330c165c62a807c123502eb
mitena <965367947@qq.com>
2021-03-10 14:27:52 +0800

    Initial commit

---
+++ testcase/test_case1.py
@@ -0,0 +1,12 @@
+import requests
+import pytest
+import allure
+@allure.feature("类的说明")
+class Testjekinss:
+    @allure.story("用例的说明")
+    def test_getreq(self):
+        url = 'http://spcare-api.medbanks-test.com/api/trading-product/product/hr/spu?channelCode=3988c7f88ebcb58c&spuCode=00001057'
+        res = requests.get(url=url)
+        print(res.json())
+        assert res.json()["message"] == "成功!"
+
